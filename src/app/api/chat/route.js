1ef405d4f954e7eee5e73c1965a9faf2c49304f5
Gaurav Dhyani <gauravdhyani90@gmail.com>
2024-06-17 22:01:31 +0530

    changed data model

--- src/app/api/chat/route.js
+++ src/app/api/chat/route.js
@@ -1,33 +1,31 @@
-import { mistral } from '@ai-sdk/mistral';
+import { mistral } from "@ai-sdk/mistral";
+import { generateText } from "ai";
+import { NextResponse } from "next/server";
 
+// const { text } = await generateText({
+//     model: mistral('mistral-large-latest'),
+//     prompt: 'Write a vegetarian lasagna recipe for 4 people.',
+//   });
 
-import { generateText } from 'ai';
-import { NextResponse } from 'next/server';
-
-const { text } = await generateText({
-    model: mistral('mistral-large-latest'),
-    prompt: 'Write a vegetarian lasagna recipe for 4 people.',
-  });
-
-export function GET(){
-    const data={
-        data:"Data1",
-        test:text,
-        version:1
-    }
-    return NextResponse.json(data)
+export function GET() {
+    const data = {
+        data: "Data1",
+        test: text,
+        version: 1,
+    };
+    return NextResponse.json(data);
 }
 
-export async function POST(request){
-    const req = await request.json()
+export async function POST(request) {
+    const req = await request.json();
     const { text } = await generateText({
-        model: mistral('mistral-large-latest'),
+        model: mistral("mistral-large-latest"),
         prompt: req.text,
-      });
+    });
     const data = {
-        data:"Data2",
-        test:text,
-        version:1
-    }
-    return NextResponse.json(data)
+        data: "Data2",
+        test: text,
+        version: 1,
+    };
+    return NextResponse.json(data);
 }
